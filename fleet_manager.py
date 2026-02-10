0438cd13c51c10bc1cba2dc7c4e2892c4beea712
tmw10 <tmw10@kent.ac.uk>
2026-02-10 13:04:49 +0000

    Crew Database

---
+++ fleet_manager.py
@@ -0,0 +1,6 @@
+#Crew database
+names = ["Picard", "Riker", "Data", "Worf", "Geordi"]
+ranks = ["Captain", "Commander", "Lt. Commander", "Lieutenant", "Lieutenant"]
+divisions = ["Command", "Command", "Operations", "Security", "Engineering"]
+ids = [101, 102, 103, 104, 105]
+
