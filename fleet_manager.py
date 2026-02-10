8e31fa74c789f8c2274b022efaaa0245997414dc
tmw10 <tmw10@kent.ac.uk>
2026-02-10 13:06:20 +0000

    initialize database

--- fleet_manager.py
+++ fleet_manager.py
@@ -4,3 +4,5 @@ ranks = ["Captain", "Commander", "Lt. Commander", "Lieutenant", "Lieutenant"]
 divisions = ["Command", "Command", "Operations", "Security", "Engineering"]
 ids = [101, 102, 103, 104, 105]
 
+def initialize_database():
+    return names, ranks, divisions, ids
