1fffe2beb25bc2a8b1fe29360517d5f4e26ac32f
tmw10 <tmw10@kent.ac.uk>
2026-02-10 12:55:05 +0000

    line 54

--- old_system.py
+++ old_system.py
@@ -44,21 +44,16 @@ def run_system_monolith():
             
         elif opt == "3":
             rem = input("Name to remove: ")
-           
-            idx = n.index(rem)
-            n.pop(idx)
-            r.pop(idx)
-            d.pop(idx)
-            print("Removed.")
-            
+
+
         elif opt == "4":
             print("Analyzing...")
             count = 0
             
             for rank in r:
-                if rank == "Captain" or "Commander": 
+                if rank == "Captain" or rank == "Commander": 
                     count = count + 1
-            print("High ranking officers: " + count) 
+            print("High ranking officers: " + str(count)) 
             
         elif opt == "5":
             print("Shutting down.")
